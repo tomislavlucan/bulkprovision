970b46879bd7acd5d050cf89c32318c6bee88269
rabbit on the shore <97438685+tomislavlucan@users.noreply.github.com>
2024-02-09 23:22:06 +0000

    Update test.py

--- test.py
+++ test.py
@@ -12,8 +12,8 @@ logger.setLevel(logging.INFO)
 
 ######################  SET VALUES HERE FOR YOUR ACCOUNT ##########################
 DEFAULT_REGION = 'eu-west-1'
-CSV_BUCKET= "xxxxxxxxxxx" 
-CSV_KEY= "xxxxxxxxxxx" 
+CSV_BUCKET= "workspaces-bulk" 
+CSV_KEY= "key.csv" 
 DYANMO_TABLE = "xxxxxxxxxxx"
 PRODUCT_ID= "prod-xxxxxxxxxxx" # #Bulk deploy product             
 PROVISIONINGA_ART_ID="pa-xxxxxxxxxxx" # Workspace
